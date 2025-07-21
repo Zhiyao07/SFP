9b8b54906d65fc7530a91e1187592d18339c11d7
Zhiyao07 <25072851@imail.sunway.edu.my>
2025-07-21 06:56:12 +0000

    Added conclusion

---
+++ Part1/ex05.py
@@ -0,0 +1,10 @@
+# Word variables
+part1 = 'the car '
+part2 = 'exceeded '
+part3 = 'the speed limit '
+
+# TODO: Combine the words into one sentence and display on screen
+# Make sure your sentence reads: "the car exceeded the speed limit"
+
+sentence = part1 + part2 + part3
+print(sentence)
